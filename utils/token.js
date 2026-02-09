a1ce0a338e5f44afc15e808f4353d51b16e87a31
skycatgamingespore74-hub <skycatgaming.espore74@gmail.com>
2026-02-09 21:41:01 +0100

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,1 +1,19 @@
+const crypto = require('crypto');
 
+const tokens = {}; // { token: email }
+
+function generateToken(email) {
+    const token = crypto.randomBytes(32).toString('hex');
+    tokens[token] = email;
+    return token;
+}
+
+function verifyToken(token) {
+    return tokens[token] || null;
+}
+
+function deleteToken(token) {
+    if (tokens[token]) delete tokens[token];
+}
+
+module.exports = { generateToken, verifyToken, deleteToken };
